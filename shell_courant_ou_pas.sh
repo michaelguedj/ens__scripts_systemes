8a7abf1d2e6b16ad7ca0d89b9f7e155d45f17401
michaelguedj <39969803+michaelguedj@users.noreply.github.com>
2019-11-11 13:30:23 +0100

    Add files via upload

---
+++ shell_courant_ou_pas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# dans le shell courant on a fait
+# toto="blabla"
+
+echo "$toto"
+
+# on effectue les tests suivants :
+#
+# ___$ bash shell_courant_ou_pas.sh
+#
+# ___$ source shell_courant_ou_pas.sh
+#
+# ___$ chmod +x shell_courant_ou_pas.sh
+#
+# ___$ ./shell_courant_ou_pas.sh
+#
